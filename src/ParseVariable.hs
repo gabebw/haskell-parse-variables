0b0ea01a165c2f5936310e465819f7a2c284137a
Gabe Berke-Williams <gabebw@gabebw.com>
2014-08-12 15:15:22 -0400

    Initial commit: 3 tests passing

--- src/ParseVariable.hs
+++ src/ParseVariable.hs
@@ -3,4 +3,13 @@ module ParseVariable where
 type Variable = (String, String)
 
 parseVariable :: String -> Maybe Variable
-parseVariable _ = Nothing
+parseVariable s = Just (key s, value s)
+
+key :: String -> String
+key = takeWhile (/= '=')
+
+value :: String -> String
+value = withoutEndingNewline . reverse . key . reverse
+
+withoutEndingNewline :: String -> String
+withoutEndingNewline = takeWhile (/= '\n')
