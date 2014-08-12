2ae5d51b3426d20c70ca838f1a7ee6d4524335d8
Gabe Berke-Williams <gabebw@gabebw.com>
2014-08-12 15:44:31 -0400

    Clean out quotes and "export"

--- src/ParseVariable.hs
+++ src/ParseVariable.hs
@@ -1,15 +1,35 @@
 module ParseVariable where
 
+import Data.Char (isUpper)
+
 type Variable = (String, String)
 
 parseVariable :: String -> Maybe Variable
+parseVariable (' ':xs) = Nothing
 parseVariable s = Just (key s, value s)
 
+removeJunk :: String -> String
+removeJunk = withoutQuotes . withoutEndingNewline . withoutBeginningExport
+
 key :: String -> String
-key = takeWhile (/= '=')
+key = (takeWhile notEqualsSign) . removeJunk
 
 value :: String -> String
-value = withoutEndingNewline . reverse . key . reverse
+value = reverse . takeWhile notEqualsSign . reverse . removeJunk
+
+notEqualsSign :: Char -> Bool
+notEqualsSign = (/= '=')
 
 withoutEndingNewline :: String -> String
 withoutEndingNewline = takeWhile (/= '\n')
+
+withoutQuotes :: String -> String
+withoutQuotes = filter (/='"') . filter (/= '\'')
+
+withoutBeginningExport :: String -> String
+withoutBeginningExport s
+  | startsWithExport s = drop 7 s
+  | otherwise = s
+
+startsWithExport :: String -> Bool
+startsWithExport s = "export" == take 6 s
