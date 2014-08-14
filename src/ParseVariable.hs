8ac9b68746af11725ec12f204f8cd8a3001f2954
Gabe Berke-Williams <gabebw@gabebw.com>
2014-08-13 22:28:43 -0400

    Only 3 failures!

--- src/ParseVariable.hs
+++ src/ParseVariable.hs
@@ -1,6 +1,6 @@
--- Idea: Parse string into ("Foo ", "=", "Bar") and then if there are spaces in
--- first element or third element, that's no good
 module ParseVariable where
+
+import Data.Maybe (fromJust)
 
 type Variable = (String, String)
 type Key = String
@@ -8,11 +8,14 @@ type Value = String
 
 parseVariable :: String -> Maybe Variable
 parseVariable (' ':_) = Nothing
-parseVariable s = Just (key, value)
+parseVariable s = processVariable (key withoutExport, value withoutExport)
   where
-    key = fst keyAndValue
-    value = snd keyAndValue
-    keyAndValue = splitOnEquals s
+    withoutExport = removeFromBeginningOf "export " s
+
+processVariable :: (Maybe Key, Maybe Value) -> Maybe Variable
+processVariable (Nothing, _) = Nothing
+processVariable (_, Nothing) = Nothing
+processVariable (Just k, Just v) = Just (k, v)
 
 splitOnEquals :: String -> (Key, Value)
 splitOnEquals s = (left, right)
@@ -21,10 +24,38 @@ splitOnEquals s = (left, right)
     right = withoutNewline $ reverse $ takeWhile (/= '=') $ reverse s
     withoutNewline = takeWhile (/='\n')
 
+key :: String -> Maybe Key
+key s
+  | hasSpace left = Nothing
+  | otherwise = Just left
+  where
+    left = removeFromBeginningOf "export " $ fst $ splitOnEquals s
+
+value :: String -> Maybe Value
+value (' ':_) = Nothing
+value s
+  | hasSpace s = Nothing
+  | otherwise = Just right
+  where
+    right = snd $ splitOnEquals s
+
+hasSpace :: String -> Bool
+hasSpace = (' ' `elem`)
+
 matchingQuotes :: Value -> Bool
 matchingQuotes ('\'':xs) = last xs == '\'' && not (containsSingleQuote (init xs))
 matchingQuotes ('"':xs) = last xs == '"' && not (containsDoubleQuote (init xs))
 matchingQuotes v = not ((containsDoubleQuote v) || (containsSingleQuote v))
 
+containsSingleQuote :: Value -> Bool
 containsSingleQuote s = '\'' `elem` s
+
+containsDoubleQuote :: Value -> Bool
 containsDoubleQuote s = '"' `elem` s
+
+removeFromBeginningOf :: String -> Value -> Value
+removeFromBeginningOf s v
+  | hasPrefix = drop (length s) v
+  | otherwise = v
+  where
+    hasPrefix = take (length s) v == s
